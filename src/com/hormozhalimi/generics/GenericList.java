4596be82b8b0d8194c3c908bb634eec8bdec9eea
hormozhalimi <halimihormoz@gmail.com>
2023-01-04 14:51:52 -0800

    Added User and Userlist to use it in Generic Class

--- src/com/hormozhalimi/generics/GenericList.java
+++ src/com/hormozhalimi/generics/GenericList.java
@@ -3,7 +3,6 @@ package com.hormozhalimi.generics;
 public class GenericList <T> {
     private T[] items =  (T[]) new Object[10];
     private int count;
-
 public void add(T item){
     items[count++] = item;
 }
--- src/com/hormozhalimi/generics/User.java
+++ src/com/hormozhalimi/generics/User.java
@@ -1,4 +1,22 @@
 package com.hormozhalimi.generics;
 
-public class User {
+public class User  implements Comparable <User> {
+    private int points;
+
+    public User(int points) {
+        this.points = points;
+    }
+    @Override
+    public int compareTo(User other) {
+
+        return points - other.points;
+        /*
+        if(points < o.points)
+            return -1;
+        if (points == o.points)
+            return 0;
+        return -1;
+
+         */
+    }
 }
