10f42a5083c82c6f78aeb7a71466edcf0f170713
hormozhalimi <halimihormoz@gmail.com>
2023-01-04 16:07:59 -0800

    Added User and Userlist to use it in Generic Class

--- src/com/hormozhalimi/Main.java
+++ src/com/hormozhalimi/Main.java
@@ -2,10 +2,12 @@ package com.hormozhalimi;
 
 import com.hormozhalimi.generics.GenericList;
 import com.hormozhalimi.generics.User;
+import com.hormozhalimi.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        var user1 = new User(10);
+/*
+         var user1 = new User(10);
         var user2 = new User(20);
         if(user1.compareTo(user2) < 0)
             System.out.println("user1 is smaller than user2");
@@ -13,9 +15,8 @@ public class Main {
             System.out.println("user1 is equal to user2 ");
         else
         System.out.println("user1 is greather then user2");
-
-
-
-
+ */
+    var max = Utils.max(new User(10), new User(20));
+    System.out.println(max);
     }
 }
